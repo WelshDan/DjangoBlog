b65bf9eef8141bfed17bc3b2285af579367c83e0
WelshDan <>
2023-09-12 10:42:33 +0000

    deployment commit

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,14 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
+from django.http import HttpResponseRedirect
 from .models import Post
 from .forms import CommentForm
 
 
 class PostList(generic.ListView):
     model = Post
-    query = Post.objects.filter(status=1).order_by('created_on')
-    template_name = 'index.html'
+    queryset = Post.objects.filter(status=1).order_by("-created_on")
+    template_name = "index.html"
     paginate_by = 6
 
 
@@ -16,10 +17,10 @@ class PostDetail(View):
     def get(self, request, slug, *args, **kwargs):
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
-        comments = post.comments.filter(approved=True).order_by('created_on')
+        comments = post.comments.filter(approved=True).order_by("-created_on")
         liked = False
         if post.likes.filter(id=self.request.user.id).exists():
-            liked = True
+                liked = True
 
         return render(
             request,
@@ -33,11 +34,10 @@ class PostDetail(View):
             },
         )
 
-
     def post(self, request, slug, *args, **kwargs):
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
-        comments = post.comments.filter(approved=True).order_by('created_on')
+        comments = post.comments.filter(approved=True).order_by("-created_on")
         liked = False
         if post.likes.filter(id=self.request.user.id).exists():
                 liked = True
@@ -53,14 +53,25 @@ class PostDetail(View):
         else:
             comment_form = CommentForm()
 
-            return render(
-                request,
-                "post_detail.html",
-                {
-                    "post": post,
-                    "comments": comments,
-                    "commented": True,
-                    "liked:": liked,
-                    "comment_form": CommentForm()
-                },
-            )
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "commented": True,
+                "liked:": liked,
+                "comment_form": CommentForm()
+            },
+        )
+
+class PostLike(View):	
+    	
+    def post(self, request, slug, *args, **kwargs):	
+        post = get_object_or_404(Post, slug=slug)	
+        if post.likes.filter(id=request.user.id).exists():	
+            post.likes.remove(request.user)	
+        else:	
+            post.likes.add(request.user)
+
+        return HttpResponseRedirect(reverse('post_detail', args=[slug]))
--- codestar/settings.py
+++ codestar/settings.py
@@ -14,7 +14,8 @@ import os
 from pathlib import Path
 
 import dj_database_url
-if os.path.isfile("env.py"):
+from django.contrib.messages import constants as messages
+if os.path.isfile('env.py'):
     import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -29,7 +30,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTION = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ["mb2test.herokuapp.com", "localhost",
                  "8000-welshdan-djangoblog-4oyn94iuaf.us2.codeanyapp.com"]
@@ -58,6 +61,14 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -154,3 +165,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ACCOUNT_EMAIL_VERIFICATION = 'none'
